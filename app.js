1492360abd68b26b8e506861f38f3f5bb9d8b802
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2025-10-19 21:40:05 +0300

    feat: Updated quote in app.js file with -Day's quote:-. Also correcting false statement in console.log.

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const quotes = require('./quotes');
 const app = express();
 
 app.get('/quote',(req,res)=>{
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    res.json({quote: quotes[randomIndex]});
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  // Add "Day's quote: " here
+  res.json({quote: "Day's quote: " + quotes[randomIndex]});
 });
 
 const PORT = process.env.PORT || 3000;
@@ -13,8 +14,9 @@ const PORT = process.env.PORT || 3000;
 if (require.main === module) {
     app.listen(PORT,()=>{
         console.log(`Server running on port ${PORT}.\n`+
-            `Also, this procedure was part of QuoteAPI exercise by Haaga-Helia,\n`+
-            `developed by student Kaj Peter Jansson`);
+            `Also, this procedure was part of QuoteAPI exercise by Haaga-Helia,\n`
+            +`developed by Juha Hinkkula and Teemu Havulinna.\n`
+            +`The exercise was then completed by Kaj Jansson.`);
     });
 }
 
